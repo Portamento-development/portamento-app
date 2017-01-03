ba59d9edb408a48ce0ad8adf43d21d623b3a1a0e
nsmeds <niilosmeds@gmail.com>
2017-01-03 12:13:45 -0800

    working on sequencer / nexusui

--- app/src/components/synth/synth.js
+++ app/src/components/synth/synth.js
@@ -1,4 +1,5 @@
 import Tone from 'tone';
+import nx from 'nexusui';
 import template from './synth.html';
 import styles from './synth.scss';
 
@@ -33,6 +34,37 @@ function controller() {
         }
     }).toMaster();
 
+    // const notes = ['E3', 'D3', 'C3', 'D3', 'E3', 'E3', 'E3'];
+    // var counter = 0;
+
+    // this.sequence = new Tone.Sequence(function(notes) {
+    //     // if ( prevNote) this.synth.triggerRelease(prevNote);
+    //     console.log(notes[counter]);
+    //     counter++;
+    //     // this.synth.triggerAttack(note);
+    //     // const prevNote = note;
+    // }, notes, '4n');
+
+    // Tone.Transport.start();
+    // this.sequence.start();
+    // this.sequence.loop = 8;
+    // this.sequence.loopEnd = '10s';
+
+    nx.onload = function(){
+        nx.colorize('#f5871f');
+        var matrix1 = angular.element('#matrix');
+        matrix1.col = 16;
+        matrix1.row = 4;
+        matrix1.init();
+        matrix1.draw();
+    };
+
+    // var noteNames = ['F#', 'E', 'C#', 'A'];
+
+    // var loop = new Tone.Sequence(function(time, col){
+    //     var column = 
+    // });
+
     this.noteOn = function(note) {
         console.log(this.synth);
         this.synth.triggerAttack(note);
@@ -47,7 +79,6 @@ function controller() {
         this.synth['filter'] = filter;
     };
 
-
     this.keyDown = function($event) {
         $event.preventDefault();
         const note = this.notes.find(n => n.keyCode === $event.keyCode);
@@ -59,4 +90,5 @@ function controller() {
         const note = this.notes.find(n => n.keyCode === $event.keyCode);
         this.noteOff(note.note);
     };
+
 }
