77ba9f9bf6c2ad7ece0b902b715e49a962fa257e
willnickerson <wnickers@willamette.edu>
2017-01-03 22:53:53 -0800

    basic 4x4 seqeuncer grid working

--- app/src/components/synth/synth.js
+++ app/src/components/synth/synth.js
@@ -54,55 +54,38 @@ function controller() {
         }
     }).toMaster();
 
-    // const notes = ['E3', 'D3', 'C3', 'D3', 'E3', 'E3', 'E3'];
-    // var counter = 0;
-
-    // this.sequence = new Tone.Sequence(function(notes) {
-    //     // if ( prevNote) this.synth.triggerRelease(prevNote);
-    //     console.log(notes[counter]);
-    //     counter++;
-    //     // this.synth.triggerAttack(note);
-    //     // const prevNote = note;
-    // }, notes, '4n');
 
 
-    // this.sequence.start();
-    // this.sequence.loop = 8;
-    // this.sequence.loopEnd = '10s';
+    this.updateMatrix = function(col, row) {
+        if(this.sequenceMatrix[col][row] === 1) this.sequenceMatrix[col][row] = 0;
+        else this.sequenceMatrix[col][row] = 1;
+        console.log(this.sequenceMatrix);
+    };
 
+    this.toggleSelect = function() {
+        console.log('hit');
+    };
 
-    // var noteNames = ['F#', 'E', 'C#', 'A'];
-    // const sequencematrix = [[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1]];
-    // var sequencematrix = document.getElementById('sequencematrix'); 
+    this.sequenceMatrix = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]];
 
-    var synth = this.synth;
+    const notes = ['F#3', 'E3', 'C#3', 'A3'];
+    let lastNote = null;
 
-    var loop = new Tone.Sequence(function(time, col) {
-        console.log('loop running');
-        console.log('time', time);
-        console.log('col', col);
-        synth.triggerAttack(col);
-        // var column = sequencematrix[col];
-        // for (var i = 0; i < 4; i++) {
-        //     if (column[i] === 1) {
-        //         var vel = Math.random() * 0.5 + 0.5;
-        //         this.synth.triggerAttack(noteNames[i], time, vel);
-        //     }
-        // }
-    }, ['F#3', 'E3', 'C#3', 'A3'], '4n');
+    var loop = new Tone.Sequence((time, col) => {
+        if(lastNote) {
+            this.synth.triggerRelease(lastNote);
+        }
+        let column = this.sequenceMatrix[col];
+        for(var i = 0; i < column.length; i++) {
+            if(column[i] === 1) {
+                var vel = Math.random() * 0.5 + 0.5;
+                this.synth.triggerAttack(notes[i], time, vel);
+                lastNote = notes[i];
+            }
+        }
+    }, [0, 1, 2, 3], '4n');
 
     Tone.Transport.start();
-
-    // nx.onload = function(){
-    //     console.log('nx onload hit');
-    //     nx.colorize('#0F0');
-    //     sequencematrix.col = 16;
-    //     sequencematrix.row = 4;
-    //     sequencematrix.height = 300;
-    //     sequencematrix.width = 300;
-    //     sequencematrix.init();
-    //     sequencematrix.draw();
-    // };
 
 
     this.startLoop = function() {
